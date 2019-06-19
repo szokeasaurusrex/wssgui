981e9891da543cf22aa445a8316c876c4ed94b4f
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2019-06-19 18:38:19 -0400

    Using EventEmitter in WSS

--- src/serial/WSS.js
+++ src/serial/WSS.js
@@ -1,12 +1,15 @@
-import { BAUD_RATE, START_BUZZ_COMMAND, STOP_BUZZ_COMMAND } from '../constants';
+import EventEmitter from 'events';
+import {
+  BAUD_RATE, START_BUZZ_COMMAND, STOP_BUZZ_COMMAND,
+} from '../constants';
 
 const SerialPort = window.require('serialport');
 
 
-class WSS {
+class WSS extends EventEmitter {
   constructor() {
+    super();
     this.port = null;
-    this.eventListeners = [];
     this.openFirstPort();
   }
 
@@ -22,15 +25,27 @@ class WSS {
   }
 
   setPortName(portName) {
+    // Close the port if it is open already
     if (this.port != null && this.port.isOpen) {
       this.port.close();
     }
 
+    // Create the new port
     this.port = new SerialPort(portName, {
       baudRate: BAUD_RATE,
     });
 
-    this.eventListeners.forEach(({ event, callback }) => this.port.on(event, callback));
+    // Listen to existing events
+    super.eventNames().forEach(
+      event => this.port.on(event, (...args) => super.emit(event, ...args)),
+    );
+
+    // Listen to newly added events
+    super.on('newListener', (event) => {
+      if (!super.eventNames().includes(event)) {
+        this.port.on(event, (...args) => super.emit(event, ...args));
+      }
+    });
   }
 
   getPortName() {
@@ -47,10 +62,6 @@ class WSS {
     if (this.port != null) {
       this.port.write(STOP_BUZZ_COMMAND);
     }
-  }
-
-  on(event, callback) {
-    this.eventListeners.push({ event, callback });
   }
 }
 
